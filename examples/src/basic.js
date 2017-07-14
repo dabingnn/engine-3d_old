a9ef885a292f31d8a681942772c67bb6b0c6d4e1
Johnny Wu <KarasAya@gmail.com>
2017-07-14 17:35:46 +0800

    add standard material

--- examples/src/basic.js
+++ examples/src/basic.js
@@ -2,11 +2,11 @@
   const app = window.app;
   const engine = window.engine;
 
+  const resl = engine.resl;
   const gfx = engine.gfx;
-  const renderer = engine.renderer;
-  const Node = engine.Node;
+  const { Node, StandardMaterial } = engine;
   const { vec3, color4, quat, randomRange } = engine.math;
-  const { Scene, Pass, Technique, Material, Model } = engine.renderer;
+  const { Scene, Model } = engine.renderer;
   const { box } = engine.primitives;
 
   // create mesh
@@ -17,58 +17,35 @@
   }));
 
   // create material
-  let pass = new Pass('unlit');
-  // pass.setDepth(true, true);
-  // pass.setCullMode(gfx.CULL_FRONT);
-  pass.setDepth(true, false);
-  pass.setBlend(
-    gfx.BLEND_FUNC_ADD,
-    gfx.BLEND_SRC_ALPHA, gfx.BLEND_ONE_MINUS_SRC_ALPHA,
-    gfx.BLEND_FUNC_ADD,
-    gfx.BLEND_ONE, gfx.BLEND_ONE
-  );
+  let material = new StandardMaterial({
+    // mainTexture: ???,
+    color: color4.new(1.0, 1.0, 1.0, 0.6),
+  });
+  material.setOption('useColor', true);
+  material.setOption('useTexture', true);
+  material.setOption('useSkinning', false);
+  material.blendType = engine.BLEND_NORMAL;
 
-  let technique = new Technique(
-    renderer.STAGE_TRANSPARENT,
-    [
-      { name: 'mainTexture', type: renderer.PARAM_TEXTURE_2D },
-      { name: 'color', type: renderer.PARAM_COLOR4, },
-    ], [
-      pass,
-    ]
-  );
-  let material = new Material(
-    [technique],
-    {
-      // mainTexture: ???,
-      color: color4.new(1.0, 1.0, 1.0, 0.6),
+  resl({
+    manifest: {
+      image: {
+        type: 'image',
+        src: './assets/uv_checker_02.jpg'
+      },
     },
-    {
-      useTexture: true,
-      useColor: true,
+    onDone (assets) {
+      let image = assets.image;
+      let texture = new gfx.Texture2D(app.device, {
+        width : image.width,
+        height: image.height,
+        wrapS: gfx.WRAP_CLAMP,
+        wrapT: gfx.WRAP_CLAMP,
+        mipmap: true,
+        images : [image]
+      });
+      material.setValue('mainTexture', texture);
     }
-  );
-
-  // resl({
-  //   manifest: {
-  //     image: {
-  //       type: 'image',
-  //       src: './assets/uv_checker_02.jpg'
-  //     },
-  //   },
-  //   onDone (assets) {
-  //     let image = assets.image;
-  //     let texture = new gfx.Texture2D(device, {
-  //       width : image.width,
-  //       height: image.height,
-  //       wrapS: gfx.WRAP_CLAMP,
-  //       wrapT: gfx.WRAP_CLAMP,
-  //       mipmap: true,
-  //       images : [image]
-  //     });
-  //     material.setValue('mainTexture', texture);
-  //   }
-  // });
+  });
 
   // scene
   let scene = new Scene();
