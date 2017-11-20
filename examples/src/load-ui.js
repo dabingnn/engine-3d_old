a0ca99647dc43079c267a8c48ed1e4f63a2bf4b2
JoneLau <350791117@qq.com>
2017-11-20 15:53:55 +0800

    import UI from unity-3d and create a new test for ui

---
+++ examples/src/load-ui.js
@@ -0,0 +1,51 @@
+(() => {
+  const { cc, app, dgui } = window;
+  const { resl, path } = cc;
+
+  let dobj = {
+    baseUrl: '/Users/johnny/gamedev-js/u3d-exporter/out',
+    scene: 'scene'
+  };
+
+  dgui.remember(dobj);
+  dgui.add(dobj, 'baseUrl');
+  dgui.add(dobj, 'scene');
+
+  resl({
+    manifest: {
+      assetInfos: {
+        type: 'text',
+        parser: JSON.parse,
+        src: `${dobj.baseUrl}/assets.json`
+      },
+
+      scene: {
+        type: 'text',
+        parser: JSON.parse,
+        src: `${dobj.baseUrl}/${dobj.scene}.json`
+      },
+    },
+
+    onDone(data) {
+      const sceneJson = data.scene;
+      const assetInfos = data.assetInfos;
+
+      for (let uuid in assetInfos) {
+        let info = assetInfos[uuid];
+        for (let item in info.urls) {
+          info.urls[item] = path.join(dobj.baseUrl, info.urls[item]);
+        }
+
+        app.assets.registerAsset(uuid, info);
+      }
+
+      cc.utils.parseLevel(
+        app,
+        sceneJson,
+        (err, level) => {
+          app.loadLevel(level);
+        }
+      );
+    }
+  });
+})();
