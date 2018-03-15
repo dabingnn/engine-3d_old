b912925de148a75356090353d5012fe28b1bde6f
CrzDog <dongyexin@hotmail.com>
2018-03-15 15:37:43 +0800

    add new emit shapes.
    update particle system.
    add new emit shapes.

--- playground/src/particle.js
+++ playground/src/particle.js
@@ -1,16 +1,16 @@
 
 (() => {
   const { app, cc, dgui } = window;
-  const { vec3, vec2, quat } = cc.math;
+  const { vec3, quat, toRadian } = cc.math;
 
   let camEnt = app.createEntity('camera');
-  vec3.set(camEnt.lpos, 10, 10, 10);
+  vec3.set(camEnt.lpos, 0, 10, 100);
   camEnt.lookAt(vec3.new(0, 10, 0));
   camEnt.addComp('Camera');
 
   let particleMaterial = new cc.Material();
-  particleMaterial.effect = app.assets.get('builtin-effect-particle-premultiply-blend');
-  // particleMaterial.define('USE_BILLBOARD', true);
+  particleMaterial.effect = app.assets.get('builtin-effect-particle-add');
+  particleMaterial.define('USE_BILLBOARD', true);
   const texUrls = {
     image: './assets/textures/particle.png',
   };
@@ -21,21 +21,31 @@
   let ent = app.createEntity('particle-node');
   let psys = ent.addComp('ParticleSystem');
   vec3.set(ent.lpos, 0, 10, 0);
+  // quat.rotateX(ent.lrot, ent.lrot, toRadian(90));
   psys.material = particleMaterial;
   psys.loop = true;
   psys.duration = 4;
-  psys.rateOverTime = 55;
-  psys.startLifetime = 10;
+  psys.rateOverTime = 300;
+  psys.startSpeed = 10;
+  psys.startLifetimeConst = 10;
+  psys.startColorType = 'randomColor';
+  psys.startSpeedType = 'randomBetweenTwoConstants';
+  psys.startSpeedConstMin = 2;
+  psys.startSpeedConstMax = 12;
+  psys.startSizeType = 'randomBetweenTwoConstants';
+  psys.startSizeConstMin = 0.5;
+  psys.startSizeConstMax = 2.5;
   psys.startDelay = 1;
   psys.simulationSpeed = 1;
-  //psys.gravityModifier = 9;
+  psys.gravityModifier = 2;
   psys._shape.boxX = 10;
   psys._shape.boxY = 10;
   psys._shape.boxZ = 10;
+  psys._shape.emitFrom = 'edge';
   psys.play();
-
 
   app.on('tick', () => {
     // do something here...
+    // quat.rotateY(ent.lrot, ent.lrot, app.deltaTime);
   });
 })();
