0c0295b1ae05ce2b3d55c3cbcd6911a7bcb31ffd
CrzDog <dongyexin@hotmail.com>
2018-03-21 10:35:58 +0800

    add particle rotation, update particle sample.
    fix bug.
    add particle rotation.
    small update.
    fix bug.

--- playground/src/particle.js
+++ playground/src/particle.js
@@ -1,10 +1,76 @@
 
 (() => {
   const { app, cc, dgui } = window;
-  const { vec3, quat, toRadian } = cc.math;
+  const { vec3, quat } = cc.math;
+
+  let dobj = {
+    emitShape: 'circle',
+    gravity: 0.0,
+    loop: true,
+    duration: 10,
+    rateOverTime: 100,
+    startDelay: 1,
+    startSpeedMin: 2,
+    startSpeedMax: 8,
+    startSizeMin: 0.5,
+    startSizeMax: 2.5,
+    startLifetime: 8,
+    startRotation: 0,
+    billboard: true
+  };
+  dgui.remember(dobj);
+  dgui.add(dobj, 'emitShape').onFinishChange(() => {
+    psys.shapeType = dobj.emitShape;
+    if (dobj.emitShape === 'sphere' || dobj.emitShape === 'hemisphere') {
+      psys._shape.radius = 10;
+      psys._shape.emitFromShell = true;
+    } else if (dobj.emitShape === 'circle') {
+      psys._shape.radius = 10;
+      psys._shape.emitFromEdge = true;
+    } else if (dobj.emitShape === 'box') {
+      psys._shape.boxX = psys._shape.boxY = psys._shape.boxZ = 10;
+      psys._shape.emitFrom = 'shell';
+    }
+  });
+  dgui.add(dobj, 'gravity').onFinishChange(() => {
+    psys.gravityModifier = dobj.gravity;
+  });
+  dgui.add(dobj, 'loop').onFinishChange(() => {
+    psys.loop = dobj.loop;
+  });
+  dgui.add(dobj, 'duration').onFinishChange(() => {
+    psys.duration = dobj.duration;
+  });
+  dgui.add(dobj, 'rateOverTime').onFinishChange(() => {
+    psys.rateOverTime = dobj.rateOverTime;
+  });
+  dgui.add(dobj, 'startDelay').onFinishChange(() => {
+    psys.startDelay = dobj.startDelay;
+  });
+  dgui.add(dobj, 'startSpeedMin').onFinishChange(() => {
+    psys.startSpeedConstMin = dobj.startSpeedMin;
+  });
+  dgui.add(dobj, 'startSpeedMax').onFinishChange(() => {
+    psys.startSpeedConstMax = dobj.startSpeedMax;
+  });
+  dgui.add(dobj, 'startSizeMin').onFinishChange(() => {
+    psys.startSizeConstMin = dobj.startSizeMin;
+  });
+  dgui.add(dobj, 'startSizeMax').onFinishChange(() => {
+    psys.startSizeConstMax = dobj.startSizeMax;
+  });
+  dgui.add(dobj, 'startLifetime').onFinishChange(() => {
+    psys.startLifetimeConst = dobj.startLifetime;
+  });
+  dgui.add(dobj, 'startRotation').onFinishChange(() => {
+    psys.startRotationConst = dobj.startRotation;
+  });
+  dgui.add(dobj, 'billboard').onFinishChange(() => {
+    psys.material.define('USE_BILLBOARD', dobj.billboard);
+  });
 
   let camEnt = app.createEntity('camera');
-  vec3.set(camEnt.lpos, 0, 10, 100);
+  vec3.set(camEnt.lpos, 0, 10, 80);
   camEnt.lookAt(vec3.new(0, 10, 0));
   camEnt.addComp('Camera');
 
@@ -20,28 +86,23 @@
 
   let ent = app.createEntity('particle-node');
   let psys = ent.addComp('ParticleSystem');
-  vec3.set(ent.lpos, 0, 10, 0);
-  // quat.rotateX(ent.lrot, ent.lrot, toRadian(90));
+  vec3.set(ent.lpos, 0, 0, 0);
   psys.material = particleMaterial;
   psys.loop = true;
-  psys.duration = 4;
-  psys.rateOverTime = 300;
-  psys.startSpeed = 10;
-  psys.startLifetimeConst = 10;
+  psys.duration = 10;
+  psys.rateOverTime = 100;
+  psys.startLifetimeConst = 8;
   psys.startColorType = 'randomColor';
   psys.startSpeedType = 'randomBetweenTwoConstants';
   psys.startSpeedConstMin = 2;
-  psys.startSpeedConstMax = 12;
+  psys.startSpeedConstMax = 8;
   psys.startSizeType = 'randomBetweenTwoConstants';
   psys.startSizeConstMin = 0.5;
   psys.startSizeConstMax = 2.5;
   psys.startDelay = 1;
   psys.simulationSpeed = 1;
-  psys.gravityModifier = 2;
-  psys._shape.boxX = 10;
-  psys._shape.boxY = 10;
-  psys._shape.boxZ = 10;
-  psys._shape.emitFrom = 'edge';
+  psys.gravityModifier = 0;
+  psys.shapeType = 'circle';
   psys.play();
 
   app.on('tick', () => {
