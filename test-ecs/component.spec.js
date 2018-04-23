64cb6a742435a2e56ae7171c5f176395cffdf0ac
shay_chen <641278529@qq.com>
2018-04-23 15:09:18 +0800

    Add ecs test to engine-3d

---
+++ test-ecs/component.spec.js
@@ -0,0 +1,776 @@
+const tap = require('tap');
+const { App, Component } = require('./dist/ecs');
+
+tap.test('component', t => {
+
+  tap.test('onInit', t => {
+    let app = new App();
+
+    tap.test('init component during entity.addComp()', t => {
+      let fooInitCount = 0;
+      let barInitCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          fooInitCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          barInitCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+
+      t.equal(fooInitCount, 1);
+      t.equal(barInitCount, 1);
+
+      t.end();
+    });
+
+    tap.test('init component during entity.clone()', t => {
+      let fooInitCount = 0;
+      let barInitCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          fooInitCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          barInitCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+      t.equal(fooInitCount, 1);
+      t.equal(barInitCount, 1);
+
+      let ent2 = ent1.clone();
+      app.tick();
+
+      t.equal(fooInitCount, 2);
+      t.equal(barInitCount, 2);
+
+      t.end();
+    });
+
+    tap.test('init component during entity.deepClone()', t => {
+      let fooInitCount = 0;
+      let barInitCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          fooInitCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onInit() {
+          barInitCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      let ent1_1_1 = app.createEntity('ent1_1_1');
+      ent1_1.setParent(ent1);
+      ent1_1_1.setParent(ent1_1);
+      app.tick();
+
+      ent1_1.addComp('Foo');
+      ent1_1_1.addComp('Bar');
+      t.equal(fooInitCount, 1);
+      t.equal(barInitCount, 1);
+
+      let ent2 = ent1.deepClone();
+      app.tick();
+
+      t.equal(fooInitCount, 2);
+      t.equal(barInitCount, 2);
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  tap.test('onEnable', t => {
+    let app = new App();
+
+    tap.test('create component during entity.addComp()', t => {
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+
+      t.equal(fooEnableCount, 1);
+      t.equal(barEnableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('entity.enabled = true', t => {
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+
+      t.equal(fooEnableCount, 1);
+      t.equal(barEnableCount, 1);
+
+      ent1.enabled = false;
+      ent1.enabled = true;
+
+      t.equal(fooEnableCount, 2);
+      t.equal(barEnableCount, 2);
+
+      t.end();
+    });
+
+    tap.test('move the entity from disabled parent to a enabled parent', t => {
+
+      /**
+       * [o] ent0
+       * [x] ent1
+       *  |- [o] ent1_1 <= Foo
+       *      |- [o] ent1_1_1 <= Bar
+       *
+       * ent1_1.setParent(ent0)
+       */
+
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      let ent1_1_1 = app.createEntity('ent1_1_1');
+      ent1.enabled = false;
+      ent1_1.setParent(ent1);
+      ent1_1_1.setParent(ent1_1);
+      app.tick();
+
+      ent1_1.addComp('Foo');
+      ent1_1_1.addComp('Bar');
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 0);
+
+      ent1_1.setParent(ent0);
+
+      t.equal(fooEnableCount, 1);
+      t.equal(barEnableCount, 1);
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  tap.test('onDisable', t => {
+    let app = new App();
+
+    tap.test('add Component to a new entity and set Component.enabled = false', t => {
+      let fooDisableCount = 0;
+      let fooEnableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+      app.registerClass('Foo', Foo);
+
+      let ent1 = app.createEntity('Entity1');
+      let foo = ent1.addComp('Foo');
+      foo.enabled = false;
+      app.tick();
+
+      t.equal(fooEnableCount, 0);
+      t.equal(fooDisableCount, 0);
+
+      t.end();
+    });
+
+    tap.test('add Component to exists entity and set Component.enabled = false', t => {
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      let foo = ent1.addComp('Foo');
+      let bar = ent1.addComp('Bar');
+
+      foo.enabled = false;
+      bar.enabled = false;
+
+      t.equal(fooDisableCount, 1);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('comp.destroy() during entity.distroy()', t => {
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+
+      ent1.destroy();
+
+      t.equal(fooDisableCount, 1);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('entity.enabled = false', t => {
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent1 = app.createEntity('Entity1');
+      app.tick();
+
+      ent1.addComp('Foo');
+      ent1.addComp('Bar');
+
+      ent1.enabled = false;
+
+      t.equal(fooDisableCount, 1);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('move the entity from enable parent to a disable parent', t => {
+
+      /**
+       * [x] ent0
+       * [o] ent1
+       *  |- [o] ent1_1 <= Foo
+       *      |- [o] ent1_1_1 <= Bar
+       *
+       * ent1_1.setParent(ent0)
+       */
+
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Foo {
+        constructor() {
+          super();
+        }
+
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      let ent1_1_1 = app.createEntity('ent1_1_1');
+      ent0.enabled = false;
+      ent1_1.setParent(ent1);
+      ent1_1_1.setParent(ent1_1);
+      app.tick();
+
+      ent1_1.addComp('Foo');
+      ent1_1_1.addComp('Bar');
+
+      ent1_1.setParent(ent0);
+
+      t.equal(fooDisableCount, 1);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  tap.test('change the entity status when entity has different status components', t => {
+    let app = new App();
+
+    tap.test('ent0.enabled change from true to false', t => {
+
+      /**
+       *  [o] ent0 <= (Foo.enabled = false)
+       *  [o] ent0 <= (Bar.enabled = true)
+       *
+       * ent0.enabled change to false
+       */
+
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+          this._enabled = false;
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      app.tick();
+
+      ent0.addComp('Foo');
+      ent0.addComp('Bar');
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 0);
+
+      ent0.enabled = false;
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('ent0.enabled change from false to true', t => {
+
+      /**
+       *  [o] ent0 <= (Foo.enabled = false)
+       *  [o] ent0 <= (Bar.enabled = true)
+       *
+       * ent0.enabled change from false to true
+       */
+
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+          this._enabled = false;
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      app.tick();
+
+      ent0.addComp('Foo');
+      ent0.addComp('Bar');
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 0);
+
+      ent0.enabled = false;
+      ent0.enabled = true;
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 2);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('move the entity which has enabled false components from enable parent to a disable parent', t => {
+
+      /**
+       * [x] ent0
+       * [o] ent1
+       *  |- [o] ent1_1 <= (Foo.enabled = false)
+       *  |- [o] ent1_1 <= (Bar.enabled = true)
+       *
+       * ent1_1.setParent(ent0)
+       */
+
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+          this._enabled = false;
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      ent0.enabled = false;
+      ent1_1.setParent(ent1);
+      app.tick();
+
+      ent1_1.addComp('Foo');
+      ent1_1.addComp('Bar');
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 0);
+
+      ent1_1.setParent(ent0);
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 1);
+
+      t.end();
+    });
+
+    tap.test('move the entity which has enabled false components from enable parent to a disable parent', t => {
+
+      /**
+       * [o] ent0
+       * [x] ent1
+       *  |- [o] ent1_1 <= (Foo.enabled = false)
+       *  |- [o] ent1_1 <= (Bar.enabled = true)
+       *
+       * ent1_1.setParent(ent0)
+       */
+
+      let fooEnableCount = 0;
+      let barEnableCount = 0;
+      let fooDisableCount = 0;
+      let barDisableCount = 0;
+
+      class Foo extends Component {
+        constructor() {
+          super();
+          this._enabled = false;
+        }
+
+        onEnable() {
+          fooEnableCount += 1;
+        }
+        onDisable() {
+          fooDisableCount += 1;
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+
+        onEnable() {
+          barEnableCount += 1;
+        }
+        onDisable() {
+          barDisableCount += 1;
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      let ent0 = app.createEntity('ent0');
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      ent1.enabled = false;
+      ent1_1.setParent(ent1);
+      app.tick();
+
+      ent1_1.addComp('Foo');
+      ent1_1.addComp('Bar');
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 0);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 0);
+
+      ent1_1.setParent(ent0);
+
+      t.equal(fooEnableCount, 0);
+      t.equal(barEnableCount, 1);
+      t.equal(fooDisableCount, 0);
+      t.equal(barDisableCount, 0);
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  t.end();
+});
---
+++ test-ecs/system.spec.js
@@ -0,0 +1,271 @@
+const tap = require('tap');
+const { App, System, Component } = require('./dist/ecs');
+
+tap.test('system', t => {
+  let app = new App();
+
+  tap.test('system reference', t => {
+    tap.test('system reference during entity.addComp()', t => {
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class FooSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      class BarSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      app.registerSystem('foo.sys', FooSystem, 'Foo');
+      app.registerSystem('bar.sys', BarSystem, 'Bar');
+      let ent1 = app.createEntity('ent1');
+      app.tick();
+
+      let foo = ent1.addComp('Foo');
+      app.tick();
+
+      t.equal(foo._system._id, 'foo.sys');
+
+      let bar = ent1.addComp('Bar');
+      app.tick();
+
+      t.equal(bar._system._id, 'bar.sys');
+
+      t.end();
+    });
+
+    tap.test('system referenced during entity.clone()', t => {
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class FooSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      class BarSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      app.registerSystem('foo.sys', FooSystem, 'Foo');
+      app.registerSystem('bar.sys', BarSystem, 'Bar');
+      let ent1 = app.createEntity('ent1');
+      app.tick();
+
+      let foo = ent1.addComp('Foo');
+      let bar = ent1.addComp('Bar');
+      app.tick();
+
+      t.equal(foo._system._id, 'foo.sys');
+      t.equal(bar._system._id, 'bar.sys');
+
+      let ent2 = ent1.clone();
+      app.tick();
+
+      t.equal(ent2.getComp('Foo')._system._id, 'foo.sys');
+      t.equal(ent2.getComp('Bar')._system._id, 'bar.sys');
+
+      t.end();
+    });
+
+    tap.test('system referenced during entity.deepClone()', t => {
+
+      /**
+      * ent1 <= Foo
+      *  |- ent1_1 <= Bar
+      *
+      * ent1.deepClone()
+      */
+
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class FooSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      class BarSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      app.registerSystem('foo.sys', FooSystem, 'Foo');
+      app.registerSystem('bar.sys', BarSystem, 'Bar');
+      let ent1 = app.createEntity('ent1');
+      let ent1_1 = app.createEntity('ent1_1');
+      ent1_1.setParent(ent1);
+      app.tick();
+
+      let foo = ent1.addComp('Foo');
+      let bar = ent1_1.addComp('Bar');
+      app.tick();
+
+      t.equal(foo._system._id, 'foo.sys');
+      t.equal(bar._system._id, 'bar.sys');
+
+      let ent2 = ent1.deepClone();
+      app.tick();
+
+      t.equal(ent2.getComp('Foo')._system._id, 'foo.sys');
+      t.equal(ent2.children[0].getComp('Bar')._system._id, 'bar.sys');
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  tap.test('system dereference', t => {
+
+    tap.test('remove component during component.destroy()', t => {
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class FooSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      class BarSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      app.registerSystem('foo.sys', FooSystem, 'Foo');
+      app.registerSystem('bar.sys', BarSystem, 'Bar');
+      let ent1 = app.createEntity('ent1');
+      app.tick();
+
+      let foo1 = ent1.addComp('Foo');
+
+      foo1.destroy();
+      app.tick();
+
+      t.equal(foo1._system, null);
+
+      let ent2 = app.createEntity('ent2');
+      app.tick();
+      let foo2 = ent2.addComp('Foo');
+      let bar2 = ent2.addComp('Bar');
+
+      bar2.destroy();
+      app.tick();
+
+      t.equal(foo2._system._id, 'foo.sys');
+      t.equal(bar2._system, null);
+
+      foo2.destroy();
+      app.tick();
+
+      t.equal(foo2._system, null);
+
+      t.end();
+    });
+
+    tap.test('remove component during entity.destroy()', t => {
+      class Foo extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class Bar extends Component {
+        constructor() {
+          super();
+        }
+      }
+
+      class FooSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      class BarSystem extends System {
+        constructor() {
+          super();
+        }
+      }
+
+      app.registerClass('Foo', Foo);
+      app.registerClass('Bar', Bar);
+      app.registerSystem('foo.sys', FooSystem, 'Foo');
+      app.registerSystem('bar.sys', BarSystem, 'Bar');
+
+      let ent1 = app.createEntity('ent1');
+      app.tick();
+
+      let foo = ent1.addComp('Foo');
+      let bar = ent1.addComp('Bar');
+
+      ent1.destroy();
+      app.tick();
+
+      t.equal(foo._system, null);
+      t.equal(bar._system, null);
+
+      t.end();
+    });
+
+    t.end();
+  });
+
+  t.end();
+});
