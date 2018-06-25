4407c54493d0ff1cdd8e91eb08c1ad300f5d7cd2
JoneLau <350791117@qq.com>
2018-06-25 17:29:04 +0800

    modify to new ecs and screen drag event

--- playground/src/edit-box.js
+++ playground/src/edit-box.js
@@ -1,6 +1,5 @@
 (() => {
   const { cc, app } = window;
-  const { resl, path } = cc;
   const { color4, vec3 } = cc.math;
 
   let camEnt = app.createEntity('camera');
@@ -10,32 +9,31 @@
 
   let screen = app.createEntity('screen');
   screen.addComp('Screen');
-  let screenWidget = screen.getComp('Widget');
 
   let ent = app.createEntity('entity');
   ent.setParent(screen);
   let sprite = ent.addComp('Image');
-  sprite._color = color4.create();
+  sprite.color = color4.create();
   sprite.setSize(350, 80);
   let entEditor = ent.addComp('EditBox');
-  entEditor._background = sprite;
-  entEditor._transition = 'color';
-  entEditor._transitionColors.normal = color4.new(1, 1, 1, 1);
-  entEditor._transitionColors.highlight = color4.new(0.3, 1, 1, 1);
-  entEditor._transitionColors.pressed = color4.new(0.8, 0.8, 0.8, 1);
-  entEditor._transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
+  entEditor.background = ent;
+  entEditor.transition = 'color';
+  entEditor.transitionColors.normal = color4.new(1, 1, 1, 1);
+  entEditor.transitionColors.highlight = color4.new(0.3, 1, 1, 1);
+  entEditor.transitionColors.pressed = color4.new(0.8, 0.8, 0.8, 1);
+  entEditor.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
   entEditor._updateState();
 
   let input = app.createEntity('input');
   input.setParent(ent);
   let inputTextComp = input.addComp('Text');
-  inputTextComp._color = color4.new(0, 0, 0, 1);
+  inputTextComp.color = color4.new(0, 0, 0, 1);
   inputTextComp.setSize(-10, -10);
   inputTextComp.setAnchors(0, 0, 1, 1);
 
-  entEditor._defaultText = 'please enter here';
-  entEditor._textComp = inputTextComp;
+  entEditor.defaultText = 'please enter here';
+  entEditor.textEnt = input;
   // entEditor._contentType = 'password';
   // entEditor._lineType = 'multi-line';
-  entEditor._returnKeyType = 'submit';
+  entEditor.returnKeyType = 'submit';
 })();
